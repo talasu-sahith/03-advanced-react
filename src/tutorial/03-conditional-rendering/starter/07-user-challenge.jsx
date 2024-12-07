c494d8b1ccb087437e8d214495c00846ad7546b7
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-08 01:34:34 +0530

    User Challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,29 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from "react";
 
+const UserChallenge = () => {
+  const [user, setUser] = useState(null);
+  const login = () => {
+    setUser({ name: "Sahith" });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <>
+      <h2>user Challenge</h2>
+      <SomeComponent user={user} />
+
+      <button className="btn" onClick={user ? logout : login}>
+        {user ? "LOG OUT" : "LOG IN"}{" "}
+      </button>
+    </>
+  );
+};
+const SomeComponent = ({ user }) => {
+  return (
+    <div>
+      <h3>{user ? "Hi " + user.name + ", Please Welcome" : "Kindly Login"}</h3>
+    </div>
+  );
+};
 export default UserChallenge;
