a49ffca0692d4c8e76e1f3ff3e47152a14d6ad19
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-07 15:47:42 +0530

    useEffectBasics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
+import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects";
 import Final from "./tutorial/01-useState/final/05-useState-gotcha";
 function App() {
   return (
     <div className="container">
       <h2>Advanced React</h2>
       <Starter />
-      <Final />
+      {/* <Final /> */}
     </div>
   );
 }
--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  const sayHello = () => {
+    console.log("Hi there");
+  };
+  sayHello();
+
+  useEffect(() => {
+    console.log("this is a use Effect");
+  }, []);
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
