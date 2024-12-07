3985511f8bfc468fc00602d1dfa6f1c2f5aec78e
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-08 00:40:30 +0530

    ShortCircuitExpample2

--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitExamples = () => {
   // falsy
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
+  const [name, setName] = useState("susan");
+  const [user, setUser] = useState({ name: "john" });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return (
+    <>
+      <h2>{text || "Default Value"}</h2>
+      <h2>{name && "Default Value"}</h2>
+      <div>{user && <SomeComponent name={user.name} />}</div>
+    </>
+  );
 };
 
+const SomeComponent = ({ name }) => {
+  return <h2>Whatevery Component : {name}</h2>;
+};
 export default ShortCircuitExamples;
