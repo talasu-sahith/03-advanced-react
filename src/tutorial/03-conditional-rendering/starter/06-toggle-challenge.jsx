a788fbb669173276a2116743324d35361ec5b4ee
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-08 01:05:31 +0530

    Toggel Challenge

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isboolean, setIsboolean] = useState(true);
+
+  //   useEffect(()=>{
+  //     setIsboolean((prev) => {
+  // return !prev
+  //     })
+  //   },[])
+
+  return (
+    <>
+      <h2>toggle challenge</h2>
+      <button className="btn" onClick={() => setIsboolean((prev) => !prev)}>
+        Click Here
+      </button>
+      {/* 
+      <h4>
+        {isboolean ? (
+          <div>
+            <p>hello World</p>
+          </div>
+        ) : (
+          ""
+        )}
+      </h4> */}
+      {isboolean ? <Alert /> : ""}
+    </>
+  );
+};
+
+const Alert = () => {
+  return (
+    <>
+      <div>
+        <p>hello World</p>
+      </div>
+    </>
+  );
 };
 
 export default ToggleChallenge;
