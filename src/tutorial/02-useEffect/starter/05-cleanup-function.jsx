eb29912a7a3bdfc2e2a205edc81e4173d363e654
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-08 16:24:26 +0530

    useEffect-setInterval and clearInterval

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div>
+      <h2>Hello There</h2>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle component
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  );
+};
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log("this is random component");
+    const intID = setInterval(() => {
+      console.log("Interval");
+    }, 1000);
+    return () => {
+      clearInterval(intID);
+      console.log("clean up");
+    };
+  }, []);
 };
 
 export default CleanupFunction;
