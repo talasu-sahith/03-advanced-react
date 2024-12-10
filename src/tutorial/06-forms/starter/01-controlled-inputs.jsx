25da6e6997e7e71ec6edd8a2c81aaf9b846f8788
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-10 12:59:59 +0530

    controlled form Inputs

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,49 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email);
+    setName("");
+    setEmail("");
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <h2>Controlled Inputs</h2>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="form-input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <div className="form-row">
+          <label className="form-label" htmlFor="email">
+            email
+          </label>
+          <input
+            id="email"
+            className="form-input"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 export default ControlledInputs;
