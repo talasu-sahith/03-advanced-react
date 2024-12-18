b29ae650dba9189f57a095402b2491bb996e305f
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-18 22:35:09 +0530

    useRef Benefits

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -3,7 +3,17 @@ import { useEffect, useRef, useState } from "react";
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
   const refContainer = useRef(null);
-
+  const isMounted = useRef(false);
+  useEffect(() => {
+    refContainer.current.focus();
+  });
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+    console.log(isMounted);
+  }, [value]);
   const handleSubmit = (e) => {
     e.preventDefault();
     const name = refContainer.current.value;
