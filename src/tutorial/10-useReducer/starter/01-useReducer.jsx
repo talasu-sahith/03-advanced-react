05b17bc19709565d773934ef43f4b6718b40b23d
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-25 17:03:36 +0530

    usereducer Basics

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,29 +1,51 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useReducer } from "react";
+import { data, people } from "../../../data";
+
+const defaultState = {
+  people: data,
+};
+
+const reducer = (state, action) => {
+  if (action.type === "clearList") {
+    return { ...state, people: [] };
+  }
+  if ((action.type = "resetList")) {
+    return { ...state, people: data };
+  }
+};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  // const [people, setPeople] = React.useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
+  };
+  const clearItems = () => {
+    dispatch({ type: "clearList" });
+  };
+  const reset = () => {
+    dispatch({ type: "resetList" });
   };
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
       <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
+        className="btn"
+        style={{ marginTop: "2rem" }}
+        onClick={state.people?.length === 0 ? reset : clearItems}
       >
-        clear items
+        {state.people?.length === 0 ? `reset` : `clear items`}
       </button>
     </div>
   );
